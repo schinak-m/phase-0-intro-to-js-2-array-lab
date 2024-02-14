f2b27abe74b137dd57e70644eaba24b59eb90602
schinak-m <shinak.x3@gmail.com>
2024-02-14 13:01:51 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    let catsArr = [...cats, name]
+    return catsArr
+}
+
+function prependCat(name){
+    let catsArr = [name, ...cats]
+    return catsArr
+}
+
+function removeLastCat(){
+    let catsArr = cats.slice(0, cats.length - 1)
+    return catsArr
+}
+
+function removeFirstCat(){
+    let catsArr = cats.slice(1, cats.length)
+    return catsArr 
+}
